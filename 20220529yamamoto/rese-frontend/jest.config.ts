3f42f7cd56c9e161aee8790151da045857ed05a7
AmpinNoHito <kazuma.yamamoto819@gmail.com>
2022-07-12 19:18:41 +0900

    テスト用環境構築

---
+++ 20220529yamamoto/rese-frontend/jest.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '^~/(.*)$': '<rootDir>/$1',
+    '^vue$': 'vue/dist/vue.common.js',
+  },
+  moduleFileExtensions: ['ts', 'js', 'vue', 'json'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.js$': 'babel-jest',
+    '.*\\.(vue)$': 'vue-jest',
+  },
+}
+
+export default config;
