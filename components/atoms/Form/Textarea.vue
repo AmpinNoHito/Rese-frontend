47dbb256cbe29f48a81be48ac61ab0d825b6b46d
AmpinNoHito <kazuma.yamamoto819@gmail.com>
2022-07-22 12:59:00 +0900

    inputとtextareaのフォントを統一

--- components/atoms/Form/Textarea.vue
+++ components/atoms/Form/Textarea.vue
@@ -1,5 +1,5 @@
 <template>
-  <textarea cols="30" rows="10" :value="textValue" @input="atInput($event.target.value)"></textarea>
+  <textarea cols="30" rows="6" :value="textValue" @input="atInput($event.target.value)"></textarea>
 </template>
 
 <script lang="ts">
